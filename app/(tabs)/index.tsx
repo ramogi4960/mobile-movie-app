6864cfe0c2acf5e2b58ba58ee09043d0c053b901
Kevin <kramzyvine@icloud.com>
2025-08-10 00:03:55 +0300

    Added navigation capability with Tabs from expo-router

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-[red]">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-[red]">This is the Home Screen</Text>
     </View>
   );
 }
