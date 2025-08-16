cca26a5a82c07b2deeabca4072eaa3309e980e6a
Kevin <kramzyvine@icloud.com>
2025-08-16 23:26:11 +0300

    Added search capability and styling to the Search screen.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,9 +1,101 @@
-import { Text, View } from "react-native";
+import MovieCard from "@/components/movieCard";
+import SearchBar from "@/components/searchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 
 export default function Index() {
+
+  const [ searchQuery, setSearchQuery ] = useState<string>('');
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+    refetch,
+    reset
+  }  = useFetch(() => fetchMovies({
+    query: searchQuery
+  }), false)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await refetch();
+      } else {
+        reset();
+      }
+    }, 600);
+
+    return () => clearTimeout(timeoutId);
+
+  }, [searchQuery])
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-[red]">This is the Search Screen</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+      source={images.bg}
+      className="absolute w-full z-0"
+      />
+
+      <FlatList
+      data={movies}
+      renderItem={({ item }) => <MovieCard {...item} />}
+      keyExtractor={(item) => item.id.toString()}
+      showsVerticalScrollIndicator={false}
+      className="px-5"
+      numColumns={3}
+      columnWrapperStyle={{
+        justifyContent: 'center',
+        gap: 16,
+        marginVertical: 16
+      }}
+      contentContainerStyle={{
+        paddingBottom: 100,
+      }}
+      ListHeaderComponent={
+        <>
+          <View className="w-full flex-row items-center justify-center mt-20 mb-5">
+            <Image
+            source={icons.logo}
+            className="w-12 h-10"
+            />
+          </View>
+
+          <View className="my-5">
+            <SearchBar
+            placeHolderText="Search movie..."
+            value={searchQuery}
+            onChangeText={(previousState: string) => setSearchQuery(previousState)}
+            />
+          </View>
+
+          { moviesLoading && (
+            <ActivityIndicator
+            size={'large'}
+            color={"#0000ff"}
+            className="my-3"
+            />
+          ) }
+          { moviesError && (
+            <Text className="text-red-500 px-5 my-3">
+              Error: {moviesError?.message}
+            </Text>
+          ) }
+
+          { !moviesLoading && !moviesError && searchQuery.trim() && movies?.length > 0 && (
+            <Text className="text-xl text-white font-bold">
+              Search results for "
+              <Text className="text-accent">{searchQuery}</Text>
+              "
+            </Text>
+          ) }
+        </>
+      }
+      />
     </View>
   );
 }
--- components/searchBar.tsx
+++ components/searchBar.tsx
@@ -4,9 +4,11 @@ import { Image, TextInput, View } from "react-native";
 interface Props {
     placeHolderText: string;
     onPress?: () => void;
+    onChangeText?: (text: string) => void;
+    value ?: string;
 }
 
-export default function SearchBar({ placeHolderText, onPress }: Props) {
+export default function SearchBar({ placeHolderText, onPress, onChangeText, value }: Props) {
     return (
         <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
             <Image
@@ -17,8 +19,8 @@ export default function SearchBar({ placeHolderText, onPress }: Props) {
             <TextInput
             onPress={onPress}
             placeholder={placeHolderText}
-            value=""
-            onChangeText={() => {}}
+            value={value}
+            onChangeText={onChangeText}
             placeholderTextColor='#a8b5db'
             className="flex-1 ml-2 text-white"
             />
